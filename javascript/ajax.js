5c97c4b98ad333ee36ae4128f2f240c84552361f
azdemissiew <ademssiew@gmail.com>
2019-05-03 12:20:24 -0700

    added the rating from the respnse and append it

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -5,14 +5,27 @@ $("#page").append("<h3>search for anything !</h3>");
 
 
 // to acces Ajax  and get the response
-var querryURL = "http://api.giphy.com/v1/gifs/search?q=&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=10";
-$.ajax({url:querryURL,
-        method: "GET"
-}).done(function(response){
-console.log(response);
-
-});
 
 
+// button function
+$("button").on("click",function(){
+    var x =$(this).data("search");
+    console.log(x);
+    var querryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=11";
+    console.log(querryURL)
 
     
+$.ajax({url:querryURL,
+    method: "GET"
+}).done(function(response){
+console.log(response.data[0].rating);
+// to get the rating from data
+$('body').append("<p>Rating:" +response.data[0].rating+"</p>");
+// to get the image from data
+
+
+})
+
+})
+
+  
