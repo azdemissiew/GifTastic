f21ee12ba07de732586a4d829e9b2cc955766246
azdemissiew <ademssiew@gmail.com>
2019-05-04 22:20:05 -0700

    added function for button

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,31 +1,51 @@
-// create an H1 for the page 
-$("#page").html("<h1> welcome to my Giphy</h1>");
-$("#page").append("<h3>search for anything !</h3>");
 
 
 
-// to acces Ajax  and get the response
 
+/* .........Globalvariables........ */
+var topics = ["messi", "Ronaldo", "kobeBryant", "Maradona", "MichaelJordan", "LebronJames", "Zidane", "DavidBeckham", "Ronaldinho", "Neymar"];
+
+for (var i = 0; i < topics.length; i++) {
+    var datas = "#" + topics[i];
+    $("#page").append("<button id= '" + topics[i] + "'>" + topics[i] + "</button>");
+    $(datas).data("search", topics[i]);
+   
+
+}
 
 // button function
-$("button").on("click",function(){
-    var x =$(this).data("search");
+$(document).ready(function() {
+$("button").on("click", function () {
+    $("#images").empty();
+    var x = $(this).data("search");
     console.log(x);
-    var querryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=11";
+    var querryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=11";
     console.log(querryURL)
 
-    
-$.ajax({url:querryURL,
-    method: "GET"
-}).done(function(response){
-console.log(response.data[0].rating);
-// to get the rating from data
-$('body').append("<p>Rating:" +response.data[0].rating+"</p>");
-// to get the image from data
 
+    $.ajax({
+        url: querryURL,
+        method: "GET"
+    }).done(function (response) {
+       
+        
+        for (var j = 0; j < response.data.length; j++) {
+           
+            // to get the rating from data
+            console.log(response.data[j].images.downsized.url);
+            $("#images").append("<p>Rating:" + response.data[j].rating + "</p>");
+            // to get the image from data
+        $("#images").append("<img src='" + response.data[j].images.downsized.url + "'>");
+        }
+
+
+    })
 
 })
-
-})
-
+});
   
+function write_name(){
+    var welcome_parra=document.getElementById("welcome");
+    var name =document.getElementById("name");
+    welcome_parra.innerHTML= name.value;
+}
