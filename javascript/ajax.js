b1d03b66c78d7808e76d91e4fdef110d6cee6dab
azdemissiew <ademssiew@gmail.com>
2019-05-08 22:40:26 -0700

    the form added

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -5,6 +5,7 @@
 /* .........Globalvariables........ */
 var topics = ["messi", "Ronaldo", "kobeBryant", "Maradona", "MichaelJordan", "LebronJames", "Zidane", "DavidBeckham", "Ronaldinho", "Neymar"];
 
+// itterates trough the topics array 
 for (var i = 0; i < topics.length; i++) {
     var datas = "#" + topics[i];
     $("#page").append("<button id= '" + topics[i] + "'>" + topics[i] + "</button>");
@@ -19,16 +20,16 @@ $("button").on("click", function () {
     $("#images").empty();
     var x = $(this).data("search");
     console.log(x);
-    var querryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=11";
+    var querryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=uUSghzLZilA2DFjfRmFUP5OIpJWcK7nH&limit=11";
     console.log(querryURL)
 
-
+    // geting the data 
     $.ajax({
         url: querryURL,
         method: "GET"
     }).done(function (response) {
        
-        
+        // looping through images 
         for (var j = 0; j < response.data.length; j++) {
            
             // to get the rating from data
@@ -36,16 +37,22 @@ $("button").on("click", function () {
             $("#images").append("<p>Rating:" + response.data[j].rating + "</p>");
             // to get the image from data
         $("#images").append("<img src='" + response.data[j].images.downsized.url + "'>");
+        
         }
 
-
+ 
     })
-
+    
 })
+
 });
+
+$("#addShow").on("click", function(event) {
+    event.preventDefault();
+    var newShow = $("#name").val().trim();
+    console.log(newShow);
+    
   
-function write_name(){
-    var welcome_parra=document.getElementById("welcome");
-    var name =document.getElementById("name");
-    welcome_parra.innerHTML= name.value;
-}
+    
+
+});
